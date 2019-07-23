7fe98956c804b7bb75d8fa48c25777947f6c983c
zcover <zerek.cover@gmail.com>
2019-07-23 14:12:48 -0700

    got the chart to work!

--- js/app.js
+++ js/app.js
@@ -161,12 +161,12 @@ function generatechart(){
             label: '# of Votes',
             data: globalVotesBank,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
@@ -176,7 +176,7 @@ function generatechart(){
                 'rgba(153, 102, 255, 1)',
                 'rgba(255, 159, 64, 1)'
             ],
-            borderWidth: 1
+            borderWidth: 20
         }]
     },
     options: {
